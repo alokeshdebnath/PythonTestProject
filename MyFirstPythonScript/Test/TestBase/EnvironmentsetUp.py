68dce9dcff396a3e37e0e1f6aacdf9920dc51a91
Datacore <Datacore@TST-LAP-0156>
2019-05-13 16:19:38 +0530

    13th may 2019

--- MyFirstPythonScript/Test/TestBase/EnvironmentsetUp.py
+++ MyFirstPythonScript/Test/TestBase/EnvironmentsetUp.py
@@ -14,7 +14,8 @@ class EnvironmentSetUp(unittest.TestCase):
 #setup for the browser setup attributes
     def setUp(self):
         #self.driver = webdriver.Chrome("C:\drivers\chromedriver.exe")
-        self.driver = webdriver.Chrome("..\Driver\chromedriver.exe")
+        #self.driver = webdriver.Chrome("..\Driver\chromedriver.exe")
+        self.driver = webdriver.Chrome("C:\\Users\\Datacore\\eclipse-workspace\\PyDevProject\\MyFirstPythonScript\\Test\\Driver\\chromedriver.exe")
         print("Run has started at :" + str (datetime.datetime.now()))
         self.driver.set_page_load_timeout(20)
         print("------------------------")
